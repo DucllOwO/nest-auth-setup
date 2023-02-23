57e75f5832297a3f8a7c7fe3404756f4f090e4d7
DucllOwO <nguyenduc147862@gmail.com>
2023-02-23 16:28:49 +0700

    setup db prisma + supabase

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,10 +1,16 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { PrismaService } from './prisma/prisma.service';
+import { UserModule } from './user/user.module';
+import { PostModule } from './post/post.module';
 
 @Module({
-  imports: [],
+  imports: [AuthModule, ConfigModule.forRoot({
+  }), UserModule, PostModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, PrismaService],
 })
-export class AppModule {}
+export class AppModule { }
---
+++ apps/api/src/auth/auth.module.ts
@@ -0,0 +1,8 @@
+import { Module } from '@nestjs/common';
+import { AuthService } from './auth.service';
+
+@Module({
+  providers: [AuthService],
+  imports: [],
+})
+export class AuthModule { }
